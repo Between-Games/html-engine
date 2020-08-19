23f8474a85495eb682b39c43c60532bc1087dba5
S-A-R-C <s-a-r-c@s-a-r-c.com>
2020-08-19 14:29:21 -0400

    Structured future functionalities for this engine

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,11 +1,186 @@
+// ╔═══════╗╔════╗╔═╗╔═══════╗╔═══════╗╔════╗╔═╗   ╔═══════╗   ╔═══════╗╔═══════╗
+// ╚══╗ ╔══╝║ ╔╗ ║║ ║╚╗ ╔══╗ ║║ ╔═════╝╚══╗ ║║ ║   ╚╗ ╔══╗ ║   ╚══╗ ╔══╝║ ╔═════╝
+//    ║ ║   ║ ║║ ║║ ║ ║ ║  ║ ║║ ╚═════╗╔══╝ ╚╝ ║    ║ ║  ║ ║      ║ ║   ║ ╚═════╗
+//    ║ ║   ║ ║║ ║║ ║ ║ ║  ║ ║║ ╔═════╝║ ╔╗ ╔══╝    ║ ║  ║ ║      ║ ║   ╚═════╗ ║
+// ╔══╝ ╚══╗║ ║║ ╚╝ ║╔╝ ╚══╝ ║║ ╚═════╗║ ║║ ╚══╗╔═╗╔╝ ╚══╝ ║╔═╗   ║ ║   ╔═════╝ ║
+// ╚═══════╝╚═╝╚════╝╚═══════╝╚═══════╝╚═╝╚════╝╚═╝╚═══════╝╚═╝   ╚═╝   ╚═══════╝
+
+// ╔═╗╔═╗╔═╗╔═══════╗╔════╗╔═╗╔═══════╗╔═══════╗╔═╗╔═╗╔═╗
+// ║ ║║ ║║ ║╚══╗ ╔══╝║ ╔╗ ║║ ║╚╗ ╔══╗ ║║ ╔═══╗ ║║ ║║ ║║ ║
+// ║ ║║ ║║ ║   ║ ║   ║ ║║ ║║ ║ ║ ║  ║ ║║ ║   ║ ║║ ║║ ║║ ║
+// ║ ║║ ║║ ║   ║ ║   ║ ║║ ║║ ║ ║ ║  ║ ║║ ║   ║ ║║ ║║ ║║ ║
+// ║ ╚╝ ╚╝ ║╔══╝ ╚══╗║ ║║ ╚╝ ║╔╝ ╚══╝ ║║ ╚═══╝ ║║ ╚╝ ╚╝ ║
+// ╚═══════╝╚═══════╝╚═╝╚════╝╚═══════╝╚═══════╝╚═══════╝
+
 export function getWindow(): object;
+
+// ╔═══════╗╔═══════╗╔═══════╗╔═╗   ╔═╗╔═══════╗╔═══════╗╔════╗╔═╗╔═══════╗
+// ╚╗ ╔══╗ ║║ ╔═══╗ ║║ ╔═════╝║ ║   ║ ║║ ╔╗ ╔╗ ║║ ╔═════╝║ ╔╗ ║║ ║╚══╗ ╔══╝
+//  ║ ║  ║ ║║ ║   ║ ║║ ║      ║ ║   ║ ║║ ║║ ║║ ║║ ╚═════╗║ ║║ ║║ ║   ║ ║
+//  ║ ║  ║ ║║ ║   ║ ║║ ║      ║ ║   ║ ║║ ║║ ║║ ║║ ╔═════╝║ ║║ ║║ ║   ║ ║
+// ╔╝ ╚══╝ ║║ ╚═══╝ ║║ ╚═════╗║ ╚═══╝ ║║ ║║ ║║ ║║ ╚═════╗║ ║║ ╚╝ ║   ║ ║
+// ╚═══════╝╚═══════╝╚═══════╝╚═══════╝╚═╝╚═╝╚═╝╚═══════╝╚═╝╚════╝   ╚═╝
+
 export function getDocument(): object;
+
+// ╔═══════╗╔═══════╗
+// ╚══╗ ╔══╝╚╗ ╔══╗ ║
+//    ║ ║    ║ ║  ║ ║
+//    ║ ║    ║ ║  ║ ║
+// ╔══╝ ╚══╗╔╝ ╚══╝ ║
+// ╚═══════╝╚═══════╝
+
+export function setElementId(element: any, id: string): boolean;
+export function isValidElementId(value: any): boolean;
+export function getElementById(id: string): any;
+
+// ╔═══════╗╔═╗      ╔═══════╗╔═══════╗╔═══════╗╔════╗╔═╗╔═══════╗
+// ║ ╔═════╝║ ║      ║ ╔═════╝║ ╔╗ ╔╗ ║║ ╔═════╝║ ╔╗ ║║ ║╚══╗ ╔══╝
+// ║ ╚═════╗║ ║      ║ ╚═════╗║ ║║ ║║ ║║ ╚═════╗║ ║║ ║║ ║   ║ ║
+// ║ ╔═════╝║ ║      ║ ╔═════╝║ ║║ ║║ ║║ ╔═════╝║ ║║ ║║ ║   ║ ║
+// ║ ╚═════╗║ ╚═════╗║ ╚═════╗║ ║║ ║║ ║║ ╚═════╗║ ║║ ╚╝ ║   ║ ║
+// ╚═══════╝╚═══════╝╚═══════╝╚═╝╚═╝╚═╝╚═══════╝╚═╝╚════╝   ╚═╝
 
 export function isElement(value: any): boolean;
 
+// ╔═╗   ╔═╗╔═══════╗╔═╗      ╔═╗   ╔═╗╔═══════╗
+// ║ ║   ║ ║║ ╔═══╗ ║║ ║      ║ ║   ║ ║║ ╔═════╝
+// ║ ╚╗ ╔╝ ║║ ╚═══╝ ║║ ║      ║ ║   ║ ║║ ╚═════╗
+// ╚╗ ║ ║ ╔╝║ ╔═══╗ ║║ ║      ║ ║   ║ ║║ ╔═════╝
+//  ║ ╚═╝ ║ ║ ║   ║ ║║ ╚═════╗║ ╚═══╝ ║║ ╚═════╗
+//  ╚═════╝ ╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝
+
+export function setValue(element: any, value): boolean;
+export function getValue(element: any): any;
+
+// ╔═══════╗╔═╗      ╔═══════╗╔═══════╗╔═══════╗╔═══════╗╔═══════╗
+// ║ ╔═════╝║ ║      ║ ╔═══╗ ║║ ╔═════╝║ ╔═════╝║ ╔═════╝║ ╔═════╝
+// ║ ║      ║ ║      ║ ╚═══╝ ║║ ╚═════╗║ ╚═════╗║ ╚═════╗║ ╚═════╗
+// ║ ║      ║ ║      ║ ╔═══╗ ║╚═════╗ ║╚═════╗ ║║ ╔═════╝╚═════╗ ║
+// ║ ╚═════╗║ ╚═════╗║ ║   ║ ║╔═════╝ ║╔═════╝ ║║ ╚═════╗╔═════╝ ║
+// ╚═══════╝╚═══════╝╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
+export function insertClasses(element: any, classNames: string[]): boolean;
+export function removeClasses(element: any, classNames: string[]): boolean;
+export function insertClass(element: any, className: string): boolean;
+export function removeClass(element: any, className: string): boolean;
+export function hasClasses(element: any, classNames: string[]): boolean;
+export function hasClass(element: any, className: string): boolean;
+export function isClassname(value: any): boolean;
+
+// ╔═══════╗╔═══════╗╔═╗   ╔═╗╔═╗      ╔═══════╗╔═══════╗
+// ║ ╔═════╝╚══╗ ╔══╝║ ║   ║ ║║ ║      ║ ╔═════╝║ ╔═════╝
+// ║ ╚═════╗   ║ ║   ║ ╚═══╝ ║║ ║      ║ ╚═════╗║ ╚═════╗
+// ╚═════╗ ║   ║ ║   ╚═════╗ ║║ ║      ║ ╔═════╝╚═════╗ ║
+// ╔═════╝ ║   ║ ║   ╔═════╝ ║║ ╚═════╗║ ╚═════╗╔═════╝ ║
+// ╚═══════╝   ╚═╝   ╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
+export function insertStyles(element: any, styles): boolean;
+export function removeStyles(element: any, styleNames): boolean;
+export function insertStyle(element: any, style): boolean;
+export function removeStyle(element: any, styleName): boolean;
+export function hasStyleValue(element: any, styleName, styleValue): boolean;
+export function hasStyle(element: any, styleName): boolean;
+export function isStyleName(value: any): boolean;
+export function isStyleValue(value: any): boolean;
+
+// ╔═══════╗╔════╗╔═╗╔═══════╗╔══════╗ ╔═╗      ╔═══════╗╔═══════╗
+// ║ ╔═════╝║ ╔╗ ║║ ║║ ╔═══╗ ║║ ╔══╗ ║ ║ ║      ║ ╔═════╝╚╗ ╔══╗ ║
+// ║ ╚═════╗║ ║║ ║║ ║║ ╚═══╝ ║║ ╚══╝ ╚╗║ ║      ║ ╚═════╗ ║ ║  ║ ║
+// ║ ╔═════╝║ ║║ ║║ ║║ ╔═══╗ ║║ ╔═══╗ ║║ ║      ║ ╔═════╝ ║ ║  ║ ║
+// ║ ╚═════╗║ ║║ ╚╝ ║║ ║   ║ ║║ ╚═══╝ ║║ ╚═════╗║ ╚═════╗╔╝ ╚══╝ ║
+// ╚═══════╝╚═╝╚════╝╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
+export function enableElement(element: any): boolean;
+export function disableElement(element: any): boolean;
+export function isEnabledElement(element: any): boolean;
+export function idDisabledElement(element: any): boolean;
+
+// ╔═══════╗╔═══════╗╔═══════╗╔═══════╗╔═╗      ╔═══════╗╔═╗   ╔═╗╔═══════╗╔═══════╗
+// ╚╗ ╔══╗ ║╚══╗ ╔══╝║ ╔═════╝║ ╔═══╗ ║║ ║      ║ ╔═══╗ ║║ ║   ║ ║║ ╔═════╝╚╗ ╔══╗ ║
+//  ║ ║  ║ ║   ║ ║   ║ ╚═════╗║ ╚═══╝ ║║ ║      ║ ╚═══╝ ║║ ╚═══╝ ║║ ╚═════╗ ║ ║  ║ ║
+//  ║ ║  ║ ║   ║ ║   ╚═════╗ ║║ ╔═════╝║ ║      ║ ╔═══╗ ║╚═════╗ ║║ ╔═════╝ ║ ║  ║ ║
+// ╔╝ ╚══╝ ║╔══╝ ╚══╗╔═════╝ ║║ ║      ║ ╚═════╗║ ║   ║ ║╔═════╝ ║║ ╚═════╗╔╝ ╚══╝ ║
+// ╚═══════╝╚═══════╝╚═══════╝╚═╝      ╚═══════╝╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝
+
+export function showElement(element: any): boolean;
+export function hideElement(element: any): boolean;
+export function isShownElement(element: any): boolean;
+export function isHiddenElement(element: any): boolean;
+
+// ╔═══════╗╔════╗╔═╗╔════╗╔═╗╔═══════╗╔═══════╗         ╔═╗   ╔═╗╔═══════╗╔═══════╗╔═╗
+// ╚══╗ ╔══╝║ ╔╗ ║║ ║║ ╔╗ ║║ ║║ ╔═════╝║ ╔═══╗ ║         ║ ║   ║ ║╚══╗ ╔══╝║ ╔╗ ╔╗ ║║ ║
+//    ║ ║   ║ ║║ ║║ ║║ ║║ ║║ ║║ ╚═════╗║ ╚═══╝ ║╔═══════╗║ ╚═══╝ ║   ║ ║   ║ ║║ ║║ ║║ ║
+//    ║ ║   ║ ║║ ║║ ║║ ║║ ║║ ║║ ╔═════╝║ ╔═╗ ╔═╝╚═══════╝║ ╔═══╗ ║   ║ ║   ║ ║║ ║║ ║║ ║
+// ╔══╝ ╚══╗║ ║║ ╚╝ ║║ ║║ ╚╝ ║║ ╚═════╗║ ║ ║ ╚═╗         ║ ║   ║ ║   ║ ║   ║ ║║ ║║ ║║ ╚═════╗
+// ╚═══════╝╚═╝╚════╝╚═╝╚════╝╚═══════╝╚═╝ ╚═══╝         ╚═╝   ╚═╝   ╚═╝   ╚═╝╚═╝╚═╝╚═══════╝
+
+export function clearInnerHtml(element: any): boolean;
+export function appendInnerHtml(element: any, innerHTML): boolean;
+export function setInnerHtml(element: any, innerHTML): boolean;
+export function getInnerHtml(element: any, innerHTML): any;
+
+// ╔═══════╗╔═╗   ╔═╗╔═══════╗╔═══════╗╔═══════╗         ╔═╗   ╔═╗╔═══════╗╔═══════╗╔═╗
+// ║ ╔═══╗ ║║ ║   ║ ║╚══╗ ╔══╝║ ╔═════╝║ ╔═══╗ ║         ║ ║   ║ ║╚══╗ ╔══╝║ ╔╗ ╔╗ ║║ ║
+// ║ ║   ║ ║║ ║   ║ ║   ║ ║   ║ ╚═════╗║ ╚═══╝ ║╔═══════╗║ ╚═══╝ ║   ║ ║   ║ ║║ ║║ ║║ ║
+// ║ ║   ║ ║║ ║   ║ ║   ║ ║   ║ ╔═════╝║ ╔═╗ ╔═╝╚═══════╝║ ╔═══╗ ║   ║ ║   ║ ║║ ║║ ║║ ║
+// ║ ╚═══╝ ║║ ╚═══╝ ║   ║ ║   ║ ╚═════╗║ ║ ║ ╚═╗         ║ ║   ║ ║   ║ ║   ║ ║║ ║║ ║║ ╚═════╗
+// ╚═══════╝╚═══════╝   ╚═╝   ╚═══════╝╚═╝ ╚═══╝         ╚═╝   ╚═╝   ╚═╝   ╚═╝╚═╝╚═╝╚═══════╝
+
+export function getOuterHtml(element: any): any;
+
+// ╔═══════╗╔═══════╗╔═══════╗╔═╗   ╔═╗╔═╗      ╔═══════╗
+// ║ ╔╗ ╔╗ ║║ ╔═══╗ ║╚╗ ╔══╗ ║║ ║   ║ ║║ ║      ║ ╔═════╝
+// ║ ║║ ║║ ║║ ║   ║ ║ ║ ║  ║ ║║ ║   ║ ║║ ║      ║ ╚═════╗
+// ║ ║║ ║║ ║║ ║   ║ ║ ║ ║  ║ ║║ ║   ║ ║║ ║      ║ ╔═════╝
+// ║ ║║ ║║ ║║ ╚═══╝ ║╔╝ ╚══╝ ║║ ╚═══╝ ║║ ╚═════╗║ ╚═════╗
+// ╚═╝╚═╝╚═╝╚═══════╝╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
 declare module 'HtmlEngine' {
     export function getWindow(): object;
+
     export function getDocument(): object;
 
+    export function setElementId(element: any, id: string): boolean;
+    export function isValidElementId(value: any): boolean;
+    export function getElementById(id: string): any;
+
     export function isElement(value: any): boolean;
+
+    export function setValue(element: any, value): boolean;
+    export function getValue(element: any): any;
+
+    export function insertClasses(element: any, classNames: string[]): boolean;
+    export function removeClasses(element: any, classNames: string[]): boolean;
+    export function insertClass(element: any, className: string): boolean;
+    export function removeClass(element: any, className: string): boolean;
+    export function hasClasses(element: any, classNames: string[]): boolean;
+    export function hasClass(element: any, className: string): boolean;
+    export function isClassname(value: any): boolean;
+
+    export function insertStyles(element: any, styles): boolean;
+    export function removeStyles(element: any, styleNames): boolean;
+    export function insertStyle(element: any, style): boolean;
+    export function removeStyle(element: any, styleName): boolean;
+    export function hasStyleValue(element: any, styleName, styleValue): boolean;
+    export function hasStyle(element: any, styleName): boolean;
+    export function isStyleName(value: any): boolean;
+    export function isStyleValue(value: any): boolean;
+
+    export function enableElement(element: any): boolean;
+    export function disableElement(element: any): boolean;
+    export function isEnabledElement(element: any): boolean;
+    export function idDisabledElement(element: any): boolean;
+
+    export function showElement(element: any): boolean;
+    export function hideElement(element: any): boolean;
+    export function isShownElement(element: any): boolean;
+    export function isHiddenElement(element: any): boolean;
+
+    export function clearInnerHtml(element: any): boolean;
+    export function appendInnerHtml(element: any, innerHTML): boolean;
+    export function setInnerHtml(element: any, innerHTML): boolean;
+    export function getInnerHtml(element: any, innerHTML): any;
+
+    export function getOuterHtml(element: any): any;
 }
--- source/index.js
+++ source/index.js
@@ -45,6 +45,10 @@ export function getDocument() { //
 // ╔══╝ ╚══╗╔╝ ╚══╝ ║
 // ╚═══════╝╚═══════╝
 
+export function setElementId(element, id) {                                 //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
 export function isValidElementId(value) {       //
     return UtilityEngine.isFilledString(value); //
 }
@@ -67,6 +71,179 @@ export function isElement(value) {      //
         value instanceof HTMLDocument;  //
 }
 
+// ╔═╗   ╔═╗╔═══════╗╔═╗      ╔═╗   ╔═╗╔═══════╗
+// ║ ║   ║ ║║ ╔═══╗ ║║ ║      ║ ║   ║ ║║ ╔═════╝
+// ║ ╚╗ ╔╝ ║║ ╚═══╝ ║║ ║      ║ ║   ║ ║║ ╚═════╗
+// ╚╗ ║ ║ ╔╝║ ╔═══╗ ║║ ║      ║ ║   ║ ║║ ╔═════╝
+//  ║ ╚═╝ ║ ║ ║   ║ ║║ ╚═════╗║ ╚═══╝ ║║ ╚═════╗
+//  ╚═════╝ ╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝
+
+export function setValue(element, value) {                              //
+    if (isValidElementId(element)) element = getElementById(element);   //
+
+    if (isElement(element)) element.value = value;                      //
+}
+
+export function getValue(element) {                                     //
+    if (isValidElementId(element)) element = getElementById(element);   //
+
+    return isElement(element) ? element.value : null;                   //
+}
+
+// ╔═══════╗╔═╗      ╔═══════╗╔═══════╗╔═══════╗╔═══════╗╔═══════╗
+// ║ ╔═════╝║ ║      ║ ╔═══╗ ║║ ╔═════╝║ ╔═════╝║ ╔═════╝║ ╔═════╝
+// ║ ║      ║ ║      ║ ╚═══╝ ║║ ╚═════╗║ ╚═════╗║ ╚═════╗║ ╚═════╗
+// ║ ║      ║ ║      ║ ╔═══╗ ║╚═════╗ ║╚═════╗ ║║ ╔═════╝╚═════╗ ║
+// ║ ╚═════╗║ ╚═════╗║ ║   ║ ║╔═════╝ ║╔═════╝ ║║ ╚═════╗╔═════╝ ║
+// ╚═══════╝╚═══════╝╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
+export function insertClasses(element, classNames) {                        //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function removeClasses(element, classNames) {                        //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function insertClass(element, className) {                           //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function removeClass(element, className) {                           //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function hasClasses(element, classNames) {                           //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function hasClass(element, className) {                              //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isClassname(value) {                                        //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+// ╔═══════╗╔═══════╗╔═╗   ╔═╗╔═╗      ╔═══════╗╔═══════╗
+// ║ ╔═════╝╚══╗ ╔══╝║ ║   ║ ║║ ║      ║ ╔═════╝║ ╔═════╝
+// ║ ╚═════╗   ║ ║   ║ ╚═══╝ ║║ ║      ║ ╚═════╗║ ╚═════╗
+// ╚═════╗ ║   ║ ║   ╚═════╗ ║║ ║      ║ ╔═════╝╚═════╗ ║
+// ╔═════╝ ║   ║ ║   ╔═════╝ ║║ ╚═════╗║ ╚═════╗╔═════╝ ║
+// ╚═══════╝   ╚═╝   ╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
+export function insertStyles(element, styles) {                             //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function removeStyles(element, styleNames) {                         //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function insertStyle(element, style) {                               //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function removeStyle(element, styleName) {                           //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function hasStyleValue(element, styleName, styleValue) {             //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function hasStyle(element, styleName) {                              //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isStyleName(value) {                                        //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isStyleValue(value) {                                       //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+// ╔═══════╗╔════╗╔═╗╔═══════╗╔══════╗ ╔═╗      ╔═══════╗╔═══════╗
+// ║ ╔═════╝║ ╔╗ ║║ ║║ ╔═══╗ ║║ ╔══╗ ║ ║ ║      ║ ╔═════╝╚╗ ╔══╗ ║
+// ║ ╚═════╗║ ║║ ║║ ║║ ╚═══╝ ║║ ╚══╝ ╚╗║ ║      ║ ╚═════╗ ║ ║  ║ ║
+// ║ ╔═════╝║ ║║ ║║ ║║ ╔═══╗ ║║ ╔═══╗ ║║ ║      ║ ╔═════╝ ║ ║  ║ ║
+// ║ ╚═════╗║ ║║ ╚╝ ║║ ║   ║ ║║ ╚═══╝ ║║ ╚═════╗║ ╚═════╗╔╝ ╚══╝ ║
+// ╚═══════╝╚═╝╚════╝╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝╚═══════╝
+
+export function enableElement(element) {                                    //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function disableElement(element) {                                   //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isEnabledElement(element) {                                 //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isDisabledElement(element) {                                //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+// ╔═══════╗╔═══════╗╔═══════╗╔═══════╗╔═╗      ╔═══════╗╔═╗   ╔═╗╔═══════╗╔═══════╗
+// ╚╗ ╔══╗ ║╚══╗ ╔══╝║ ╔═════╝║ ╔═══╗ ║║ ║      ║ ╔═══╗ ║║ ║   ║ ║║ ╔═════╝╚╗ ╔══╗ ║
+//  ║ ║  ║ ║   ║ ║   ║ ╚═════╗║ ╚═══╝ ║║ ║      ║ ╚═══╝ ║║ ╚═══╝ ║║ ╚═════╗ ║ ║  ║ ║
+//  ║ ║  ║ ║   ║ ║   ╚═════╗ ║║ ╔═════╝║ ║      ║ ╔═══╗ ║╚═════╗ ║║ ╔═════╝ ║ ║  ║ ║
+// ╔╝ ╚══╝ ║╔══╝ ╚══╗╔═════╝ ║║ ║      ║ ╚═════╗║ ║   ║ ║╔═════╝ ║║ ╚═════╗╔╝ ╚══╝ ║
+// ╚═══════╝╚═══════╝╚═══════╝╚═╝      ╚═══════╝╚═╝   ╚═╝╚═══════╝╚═══════╝╚═══════╝
+
+export function showElement(element) {                                      //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function hideElement(element) {                                      //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isShownElement(element) {                                   //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function isHiddenElement(element) {                                  //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+// ╔═══════╗╔════╗╔═╗╔════╗╔═╗╔═══════╗╔═══════╗         ╔═╗   ╔═╗╔═══════╗╔═══════╗╔═╗
+// ╚══╗ ╔══╝║ ╔╗ ║║ ║║ ╔╗ ║║ ║║ ╔═════╝║ ╔═══╗ ║         ║ ║   ║ ║╚══╗ ╔══╝║ ╔╗ ╔╗ ║║ ║
+//    ║ ║   ║ ║║ ║║ ║║ ║║ ║║ ║║ ╚═════╗║ ╚═══╝ ║╔═══════╗║ ╚═══╝ ║   ║ ║   ║ ║║ ║║ ║║ ║
+//    ║ ║   ║ ║║ ║║ ║║ ║║ ║║ ║║ ╔═════╝║ ╔═╗ ╔═╝╚═══════╝║ ╔═══╗ ║   ║ ║   ║ ║║ ║║ ║║ ║
+// ╔══╝ ╚══╗║ ║║ ╚╝ ║║ ║║ ╚╝ ║║ ╚═════╗║ ║ ║ ╚═╗         ║ ║   ║ ║   ║ ║   ║ ║║ ║║ ║║ ╚═════╗
+// ╚═══════╝╚═╝╚════╝╚═╝╚════╝╚═══════╝╚═╝ ╚═══╝         ╚═╝   ╚═╝   ╚═╝   ╚═╝╚═╝╚═╝╚═══════╝
+
+export function clearInnerHtml(element) {                                   //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function appendInnerHtml(element, innerHTML) {                       //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function setInnerHtml(element, innerHTML) {                          //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+export function getInnerHtml(element, innerHTML) {                          //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
+// ╔═══════╗╔═╗   ╔═╗╔═══════╗╔═══════╗╔═══════╗         ╔═╗   ╔═╗╔═══════╗╔═══════╗╔═╗
+// ║ ╔═══╗ ║║ ║   ║ ║╚══╗ ╔══╝║ ╔═════╝║ ╔═══╗ ║         ║ ║   ║ ║╚══╗ ╔══╝║ ╔╗ ╔╗ ║║ ║
+// ║ ║   ║ ║║ ║   ║ ║   ║ ║   ║ ╚═════╗║ ╚═══╝ ║╔═══════╗║ ╚═══╝ ║   ║ ║   ║ ║║ ║║ ║║ ║
+// ║ ║   ║ ║║ ║   ║ ║   ║ ║   ║ ╔═════╝║ ╔═╗ ╔═╝╚═══════╝║ ╔═══╗ ║   ║ ║   ║ ║║ ║║ ║║ ║
+// ║ ╚═══╝ ║║ ╚═══╝ ║   ║ ║   ║ ╚═════╗║ ║ ║ ╚═╗         ║ ║   ║ ║   ║ ║   ║ ║║ ║║ ║║ ╚═════╗
+// ╚═══════╝╚═══════╝   ╚═╝   ╚═══════╝╚═╝ ╚═══╝         ╚═╝   ╚═╝   ╚═╝   ╚═╝╚═╝╚═╝╚═══════╝
+
+export function getOuterHtml(element) {                                     //
+    throw new Error('The provided function has not yet been implemented');  //
+}
+
 // ╔═══════╗╔════╗╔═╗╔═══════╗╔═══════╗╔═══════╗╔═══════╗
 // ║ ╔═════╝╚══╗ ║║ ║║ ╔═══╗ ║║ ╔═══╗ ║║ ╔═══╗ ║╚══╗ ╔══╝
 // ║ ╚═════╗╔══╝ ╚╝ ║║ ╚═══╝ ║║ ║   ║ ║║ ╚═══╝ ║   ║ ║
@@ -78,9 +255,46 @@ export default {
     getWindow,
     getDocument,
 
+    setElementId,
     isValidElementId,
     getElementById,
 
-
     isElement,
+
+    setValue,
+    getValue,
+
+    insertClasses,
+    removeClasses,
+    insertClass,
+    removeClass,
+    hasClasses,
+    hasClass,
+    isClassname,
+
+    insertStyles,
+    removeStyles,
+    insertStyle,
+    removeStyle,
+    hasStyleValue,
+    hasStyle,
+    isStyleName,
+    isStyleValue,
+
+    enableElement,
+    disableElement,
+    isEnabledElement,
+    isDisabledElement,
+
+    showElement,
+    hideElement,
+    isShownElement,
+    isHiddenElement,
+
+    clearInnerHtml,
+    appendInnerHtml,
+    setInnerHtml,
+    getInnerHtml,
+
+    getOuterHtml,
 }
